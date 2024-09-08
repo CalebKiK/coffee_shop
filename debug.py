7a97a59974408567d90abe87b34f17609c3c6eb5
WAHU0508 <sharongikenye@gmail.com>
2024-09-08 20:12:55 +0300

    debuging file

---
+++ debug.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import ipdb
+
+from customer import Customer
+from order import Order
+from coffee import Coffee
+
+if __name__ == '__main__':
+    print("HELLO! :) let's debug")
+
+    ipdb.set_trace()
